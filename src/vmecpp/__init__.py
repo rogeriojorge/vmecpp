ecf795735e47dd68857528769d43b262040c80a4
Enrico Guiraud <eguiraud@proximafusion.com>
2024-11-21 20:10:27 +0100

    Fix import path in vmecpp/__init__.py (after we moved the C++ sources)

--- src/vmecpp/__init__.py
+++ src/vmecpp/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2024-present Proxima Fusion GmbH <info@proximafusion.com>
 #
 # SPDX-License-Identifier: MIT
-from vmecpp._vmecpp import run
+from vmecpp.cpp._vmecpp import run
 
 __all__ = ["run"]
