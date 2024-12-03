336660a0e5d7fb86fad264941fef1fc72867dc77
Enrico Guiraud <eguiraud@proximafusion.com>
2024-12-03 15:59:44 +0100

    Actually write out wout file when using the CLI API

--- src/vmecpp/__main__.py
+++ src/vmecpp/__main__.py
@@ -2,8 +2,14 @@
 #
 # SPDX-License-Identifier: MIT
 import sys
+from pathlib import Path
 
-from vmecpp._vmecpp import VmecINDATAPyWrapper, run
+import vmecpp
 
-indata = VmecINDATAPyWrapper.from_file(sys.argv[1])
-run(indata)
+input_file = Path(sys.argv[1])
+input = vmecpp.VmecInput.from_file(input_file)
+output = vmecpp.run(input)
+wout_file = Path(f"wout_{vmecpp._util.get_vmec_configuration_name(input_file)}.nc")
+output.wout.save(wout_file)
+
+print(f"\nOutput written to {wout_file}.")  # noqa: T201
