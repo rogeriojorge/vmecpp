62b1bc2b7481403aa43ae9352cb7a0889d482040
Jonathan Schilling <jons@proximafusion.com>
2025-01-08 11:41:45 +0100

    Migrate the Coil type from protobuf to a C++ struct.

--- src/vmecpp/cpp/vmecpp/common/magnetic_configuration_definition/magnetic_configuration.h
+++ src/vmecpp/cpp/vmecpp/common/magnetic_configuration_definition/magnetic_configuration.h
@@ -9,6 +9,88 @@
 
 namespace magnetics {
 
+struct Coil {
+  // a human-readable name, e.g., for plotting
+  bool has_name_ = false;
+  std::string name_;
+
+  // number of windings == multiplier for current along geometry;
+  // num_windings == 1 is assumed if this field is not populated
+  bool has_num_windings_ = false;
+  double num_windings_ = 0.0;
+
+  // objects that define the single-turn geometry of the coil
+  std::list<CurrentCarrier> current_carriers_;
+
+  // name
+  bool has_name() const { return has_name_; }
+  const std::string& name() const { return name_; }
+  void set_name(const std::string& value) {
+    name_ = value;
+    has_name_ = true;
+  }
+  void set_name(std::string&& value) {
+    name_ = std::move(value);
+    has_name_ = true;
+  }
+  void clear_name() {
+    name_.clear();
+    has_name_ = false;
+  }
+
+  // num_windings
+  bool has_num_windings() const { return has_num_windings_; }
+  double num_windings() const { return num_windings_; }
+  void set_num_windings(double value) {
+    num_windings_ = value;
+    has_num_windings_ = true;
+  }
+  void clear_num_windings() {
+    num_windings_ = 0.0;
+    has_num_windings_ = false;
+  }
+
+  // current_carriers
+  int current_carriers_size() const {
+    return static_cast<int>(current_carriers_.size());
+  }
+  const CurrentCarrier& current_carriers(int index) const {
+    auto it = current_carriers_.cbegin();
+    // No bounds checks here for brevity
+    std::advance(it, index);
+    return *it;
+  }
+  CurrentCarrier* mutable_current_carriers(int index) {
+    auto it = current_carriers_.begin();
+    // No bounds checks here for brevity
+    std::advance(it, index);
+    return &(*it);
+  }
+  CurrentCarrier* add_current_carriers() {
+    current_carriers_.emplace_back();
+    auto it = current_carriers_.end();
+    // Move to newly added element
+    --it;
+    return &(*it);
+  }
+  const std::list<CurrentCarrier>& current_carriers() const {
+    return current_carriers_;
+  }
+  std::list<CurrentCarrier>* mutable_current_carriers() {
+    return &current_carriers_;
+  }
+  void clear_current_carriers() {
+    current_carriers_.clear();
+  }
+
+  // Clear the entire structure
+  void Clear() {
+    clear_name();
+    clear_num_windings();
+    clear_current_carriers();
+  }
+}; // Coil
+
 struct SerialCircuit {
   // a human-readable name, e.g., for plotting
   bool has_name_ = false;
